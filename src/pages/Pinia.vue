95726cfdbeca9e8cada27aaebc4fa9e0deb4899a
G100 <g100my.lan229@gmail.com>
2022-02-17 21:09:18 +0800

    demo: pinia

---
+++ src/pages/Pinia.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { useCounter } from '../stores/counter'
+
+const counterStore = useCounter()
+</script>
+
+<template>
+  <div>
+    <p>{{ counterStore.counter }}</p>
+    <div>
+      <button type="button" @click="counterStore.$reset">reset</button>
+      <button type="button" @click="counterStore.increment()">increase</button>
+    </div>
+  </div>
+</template>
