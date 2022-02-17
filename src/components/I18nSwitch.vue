0552c533846fd6bb41976c85719b0c04d00f45dc
G100 <g100my.lan229@gmail.com>
2022-02-17 20:04:14 +0800

    demo: i18nSwitch

---
+++ src/components/I18nSwitch.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
+import { messages } from '../locales'
+
+const languages = Object.keys(messages)
+
+const i18n = useI18n()
+function handleLanguageChanged(e: Event) {
+  console.log(e)
+  i18n.locale.value = (e.target as HTMLSelectElement).value
+}
+</script>
+<template>
+  <div>
+    <select :value="$i18n.locale" @change="handleLanguageChanged">
+      <option v-for="language in languages">{{ language }}</option>
+    </select>
+    <p>{{ $t('hello_i18n') }}</p>
+  </div>
+</template>
