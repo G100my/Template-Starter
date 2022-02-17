2dcadeccf9b054eac17c469916e34a662632e059
G100 <g100my.lan229@gmail.com>
2022-02-17 21:09:08 +0800

    setup: pinia

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from './locales'
 import router from './router'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).use(i18n).mount('#app')
+createApp(App).use(router).use(createPinia()).use(i18n).mount('#app')
