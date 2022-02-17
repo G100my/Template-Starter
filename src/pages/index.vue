486cd8efe2d6cbad072ab74fd77b4fe7774af094
G100 <g100my.lan229@gmail.com>
2022-02-18 01:26:10 +0800

    demo: unplugin-vue-components

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import I18nSwitch from '../components/I18nSwitch.vue'
-import HelloWorld from '../components/HelloWorld.vue'
-import TestNav from '../components/TestNav.vue'
 </script>
 <template>
   <img alt="Vue logo" src="../assets/logo.png" />
